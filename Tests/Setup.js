6bdae708a3555e4000c2561ce2df387d1a24f5bd
justinphan1992 <tdat258@gmail.com>
2017-04-09 21:35:47 +0700

    Second Commit

--- Tests/Setup.js
+++ Tests/Setup.js
@@ -1,11 +1,7 @@
 import mockery from 'mockery'
 import m from 'module'
-import english from '../App/I18n/languages/english.json'
-import {keys, replace, forEach} from 'ramda'
-
 // inject __DEV__ as it is not available when running through the tests
 global.__DEV__ = true
-
 // We enable mockery and leave it on.
 mockery.enable()
 
@@ -28,25 +24,11 @@ mockery.registerMock('react-native-router-flux', {Actions: {'myScreen': () => {}
 // If you are not using '../App/I18n/english.json' for your I18n values, simply replace import english
 // with the import at the top of this file from '../App/I18n/english.json' with the I18n json file you
 // want to use, such as "import french from '../App/I18n/fr.json'" and set 'let value = french[key]`
-mockery.registerMock('react-native-i18n', {
-  t: (key, replacements) => {
-    let value = english[key]
-    if (!value) return key
-    if (!replacements) return value
-
-    forEach((r) => {
-      value = replace(`{{${r}}}`, replacements[r], value)
-    }, keys(replacements))
-    return value
-  }
-})
-
 // Mock all images for React Native
 const originalLoader = m._load
 m._load = (request, parent, isMain) => {
   if (request.match(/.jpeg|.jpg|.png|.gif$/)) {
     return { uri: request }
   }
-
   return originalLoader(request, parent, isMain)
 }
